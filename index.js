50ca75da233844deace41efa56dfb5790a768e89
Kirby T. Wu <tkirby@gmail.com>
2013-08-24 02:12:13 +0800

    fix project list; refine style

--- index.js
+++ index.js
@@ -8,69 +8,84 @@ sum = function(v, k){
   }), 0);
 };
 g0visController = function($scope, $http){
-  return $http.get('https://api.github.com/users/g0v/repos', {
+  return $http.get('https://api.github.com/orgs/g0v/repos', {
     params: {
       client_id: 'ab2376373fe2da1ccb17',
       client_secret: '6560c859d46e79f00dade30999ee62ca5052437d'
     }
-  }).success(function(prjs){
-    var i$, len$, _prj, results$ = [];
-    $scope.contributors = {};
-    $scope.contribArray = [];
-    $scope.prjs = prjs;
-    $scope.forks = sum(prjs, 'forks_count');
-    $scope.issues = sum(prjs, 'open_issues');
-    $scope.watchers = sum(prjs, 'watchers');
-    $scope.contributorCount = 0;
-    $scope.prjsAllcontrib = 0;
-    $scope.commits = 0;
-    for (i$ = 0, len$ = prjs.length; i$ < len$; ++i$) {
-      _prj = prjs[i$];
-      results$.push(fn$(_prj));
-    }
-    return results$;
-    function fn$(prj){
-      $http.get("https://api.github.com/repos/g0v/" + prj.name + "/stats/commit_activity", {
-        params: {
-          client_id: 'ab2376373fe2da1ccb17',
-          client_secret: '6560c859d46e79f00dade30999ee62ca5052437d'
-        }
-      }).success(function(d){
-        var commits, i$, len$, contrib;
-        commits = 0;
-        for (i$ = 0, len$ = d.length; i$ < len$; ++i$) {
-          contrib = d[i$];
-          commits += contrib.total;
-        }
-        prj.commits = commits;
-        return $scope.commits += commits;
+  }).success(function(orgPrjs){
+    var orgPrjsList;
+    orgPrjsList = orgPrjs.map(function(it){
+      return it.name;
+    });
+    return $http.get('https://api.github.com/users/g0v/repos', {
+      params: {
+        client_id: 'ab2376373fe2da1ccb17',
+        client_secret: '6560c859d46e79f00dade30999ee62ca5052437d'
+      }
+    }).success(function(userPrjs){
+      var prjs, i$, len$, _prj, results$ = [];
+      userPrjs = userPrjs.filter(function(it){
+        return !in$(it.name, orgPrjsList);
       });
-      return $http.get(prj.contributors_url.replace(/{.+}/, ""), {
-        params: {
-          client_id: 'ab2376373fe2da1ccb17',
-          client_secret: '6560c859d46e79f00dade30999ee62ca5052437d'
-        }
-      }).success(function(d){
-        var i$, len$, user, v, results$ = [];
-        for (i$ = 0, len$ = d.length; i$ < len$; ++i$) {
-          user = d[i$];
-          if (!(user.login in $scope.contributors)) {
-            v = {
-              name: user.login,
-              total: 0,
-              projects: []
-            };
-            $scope.contributors[user.login] = v;
-            $scope.contribArray.push(v);
-            $scope.contributorCount += 1;
+      prjs = orgPrjs.concat(userPrjs);
+      $scope.contributors = {};
+      $scope.contribArray = [];
+      $scope.prjs = prjs;
+      $scope.forks = sum(prjs, 'forks_count');
+      $scope.issues = sum(prjs, 'open_issues');
+      $scope.watchers = sum(prjs, 'watchers');
+      $scope.contributorCount = 0;
+      $scope.prjsAllcontrib = 0;
+      $scope.commits = 0;
+      for (i$ = 0, len$ = prjs.length; i$ < len$; ++i$) {
+        _prj = prjs[i$];
+        results$.push(fn$(_prj));
+      }
+      return results$;
+      function fn$(prj){
+        $http.get("https://api.github.com/repos/g0v/" + prj.name + "/stats/commit_activity", {
+          params: {
+            client_id: 'ab2376373fe2da1ccb17',
+            client_secret: '6560c859d46e79f00dade30999ee62ca5052437d'
           }
-          $scope.contributors[user.login].total += 1;
-          $scope.contributors[user.login].projects.push(prj.name);
-          results$.push($scope.prjsAllcontrib += 1);
-        }
-        return results$;
-      });
-    }
+        }).success(function(d){
+          var commits, i$, len$, contrib;
+          commits = 0;
+          for (i$ = 0, len$ = d.length; i$ < len$; ++i$) {
+            contrib = d[i$];
+            commits += contrib.total;
+          }
+          prj.commits = commits;
+          return $scope.commits += commits;
+        });
+        return $http.get(prj.contributors_url.replace(/{.+}/, ""), {
+          params: {
+            client_id: 'ab2376373fe2da1ccb17',
+            client_secret: '6560c859d46e79f00dade30999ee62ca5052437d'
+          }
+        }).success(function(d){
+          var i$, len$, user, v, results$ = [];
+          for (i$ = 0, len$ = d.length; i$ < len$; ++i$) {
+            user = d[i$];
+            if (!(user.login in $scope.contributors)) {
+              v = {
+                name: user.login,
+                total: 0,
+                projects: []
+              };
+              $scope.contributors[user.login] = v;
+              $scope.contribArray.push(v);
+              $scope.contributorCount += 1;
+            }
+            $scope.contributors[user.login].total += 1;
+            $scope.contributors[user.login].projects.push(prj.name);
+            results$.push($scope.prjsAllcontrib += 1);
+          }
+          return results$;
+        });
+      }
+    });
   });
 };
 function curry$(f, bound){
@@ -85,4 +100,9 @@ function curry$(f, bound){
     } : f;
   };
   return _curry();
+}
+function in$(x, arr){
+  var i = -1, l = arr.length >>> 0;
+  while (++i < l) if (x === arr[i] && i in arr) return true;
+  return false;
 }
--- index.ls
+++ index.ls
@@ -2,52 +2,56 @@ angular.module \g0vis, []
 
 sum = (v,k) -> v.map (-> it[k]) .reduce (+), 0
 g0visController = ($scope, $http) ->
-  $http.get \https://api.github.com/users/g0v/repos,
+  
+  $http.get \https://api.github.com/orgs/g0v/repos,
     params: 
       client_id: \ab2376373fe2da1ccb17
       client_secret: \6560c859d46e79f00dade30999ee62ca5052437d
-  .success (prjs) ->
-    $scope.contributors = {}
-    $scope.contrib-array = []
-    $scope.prjs = prjs
-    #$scope.forks = prjs.map (-> it.forks_count) .reduce (+), 0
-    #$scope.issues = prjs.map (-> it.open_issues) .reduce (+), 0
-    $scope.forks = sum prjs, \forks_count
-    $scope.issues = sum prjs, \open_issues
-    $scope.watchers = sum prjs, \watchers
-    $scope.contributor-count = 0
-    $scope.prjs-allcontrib = 0
-    $scope.commits = 0
-    for _prj in prjs
-      ( (prj) -> 
-        $http.get "https://api.github.com/repos/g0v/#{prj.name}/stats/commit_activity",
-          params: 
-            client_id: \ab2376373fe2da1ccb17
-            client_secret: \6560c859d46e79f00dade30999ee62ca5052437d
-        .success (d) -> 
-          commits = 0
-          for contrib in d
-            commits += contrib.total
-          prj.commits = commits
-          $scope.commits += commits
-        $http.get prj.contributors_url.replace(/{.+}/, ""), 
-          params: 
-            client_id: \ab2376373fe2da1ccb17
-            client_secret: \6560c859d46e79f00dade30999ee62ca5052437d
-        .success (d) ->
-          for user in d
-            if user.login not of $scope.contributors
-              v = {name: user.login, total: 0, projects: []}
-              $scope.contributors[user.login] = v
-              $scope.contrib-array .push v
-              $scope.contributor-count += 1
-            $scope.contributors[user.login].total += 1
-            $scope.contributors[user.login].projects.push prj.name
-            $scope.prjs-allcontrib += 1
-      ) _prj
+  .success (org-prjs) ->
+    org-prjs-list = org-prjs.map (->it.name)
+    $http.get \https://api.github.com/users/g0v/repos,
+      params: 
+        client_id: \ab2376373fe2da1ccb17
+        client_secret: \6560c859d46e79f00dade30999ee62ca5052437d
+    .success (user-prjs) ->
+      user-prjs = user-prjs.filter (-> it.name not in org-prjs-list )
+      prjs = org-prjs ++ user-prjs
+      $scope.contributors = {}
+      $scope.contrib-array = []
+      $scope.prjs = prjs
+      #$scope.forks = prjs.map (-> it.forks_count) .reduce (+), 0
+      #$scope.issues = prjs.map (-> it.open_issues) .reduce (+), 0
+      $scope.forks = sum prjs, \forks_count
+      $scope.issues = sum prjs, \open_issues
+      $scope.watchers = sum prjs, \watchers
+      $scope.contributor-count = 0
+      $scope.prjs-allcontrib = 0
+      $scope.commits = 0
+      for _prj in prjs
+        ( (prj) -> 
+          $http.get "https://api.github.com/repos/g0v/#{prj.name}/stats/commit_activity",
+            params: 
+              client_id: \ab2376373fe2da1ccb17
+              client_secret: \6560c859d46e79f00dade30999ee62ca5052437d
+          .success (d) -> 
+            commits = 0
+            for contrib in d
+              commits += contrib.total
+            prj.commits = commits
+            $scope.commits += commits
+          $http.get prj.contributors_url.replace(/{.+}/, ""), 
+            params: 
+              client_id: \ab2376373fe2da1ccb17
+              client_secret: \6560c859d46e79f00dade30999ee62ca5052437d
+          .success (d) ->
+            for user in d
+              if user.login not of $scope.contributors
+                v = {name: user.login, total: 0, projects: []}
+                $scope.contributors[user.login] = v
+                $scope.contrib-array .push v
+                $scope.contributor-count += 1
+              $scope.contributors[user.login].total += 1
+              $scope.contributors[user.login].projects.push prj.name
+              $scope.prjs-allcontrib += 1
+        ) _prj
 
-#window.onload = ->
-#  $.ajax \http://kcwu.csie.org/~kcwu/ircstat/g0v-count.json,
-#  .done ->
-#    #console.log it
-#  .fail -> # fallback to do something
